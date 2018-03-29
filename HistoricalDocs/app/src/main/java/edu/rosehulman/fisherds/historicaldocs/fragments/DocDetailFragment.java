2ab6d82f45a65df85a310bf5a4f4a82582df3cbb
David Fisher <fisherds@rose-hulman.edu>
2018-03-29 16:01:03 -0400

    Stubs for the DocDetailFragment

---
+++ HistoricalDocs/app/src/main/java/edu/rosehulman/fisherds/historicaldocs/fragments/DocDetailFragment.java
@@ -0,0 +1,66 @@
+package edu.rosehulman.fisherds.historicaldocs.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.rosehulman.fisherds.historicaldocs.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DocDetailFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DocDetailFragment extends Fragment {
+  // TODO: Rename parameter arguments, choose names that match
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "param1";
+  private static final String ARG_PARAM2 = "param2";
+
+  // TODO: Rename and change types of parameters
+  private String mParam1;
+  private String mParam2;
+
+
+  public DocDetailFragment() {
+    // Required empty public constructor
+  }
+
+  /**
+   * Use this factory method to create a new instance of
+   * this fragment using the provided parameters.
+   *
+   * @param param1 Parameter 1.
+   * @param param2 Parameter 2.
+   * @return A new instance of fragment DocDetailFragment.
+   */
+  // TODO: Rename and change types and number of parameters
+  public static DocDetailFragment newInstance(String param1, String param2) {
+    DocDetailFragment fragment = new DocDetailFragment();
+    Bundle args = new Bundle();
+    args.putString(ARG_PARAM1, param1);
+    args.putString(ARG_PARAM2, param2);
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      mParam1 = getArguments().getString(ARG_PARAM1);
+      mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_doc_detail, container, false);
+  }
+
+}
